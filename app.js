c80db4e19c9fbd786b33dfcf78e649e0807d0c7b
Ricky Dam <RickyDamOttawa@gmail.com>
2019-08-08 15:10:25 -0400

    Reorder redis client event emitters

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const redis = require('redis');
 
 let redisClient = redis.createClient();
-redisClient.on('error', function(err) {
-    console.error('Error: ' + err);
-});
-
 redisClient.on('connect', function() {
     console.log("Redis client connected.");
+});
+redisClient.on('error', function(err) {
+    console.error('Error: ' + err);
 });

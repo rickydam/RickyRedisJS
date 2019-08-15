50d85e0866a9e30b0a99b4e11f5125129470ceac
Ricky Dam <RickyDamOttawa@gmail.com>
2019-08-15 11:21:24 -0400

    Implement worker1 XREADGROUP, XREADGROUP BLOCK, and XACK

--- clawWorker1.js
+++ clawWorker1.js
@@ -2,7 +2,7 @@ const redis = require('redis');
 
 let redisClient = redis.createClient();
 redisClient.on('connect', function() {
-    console.log("Redis client connected.");
+    console.log("Redis client connected.\n");
 });
 redisClient.on('error', function(err) {
     console.error('Error: ' + err);
@@ -10,76 +10,65 @@ redisClient.on('error', function(err) {
 
 class ClawWorker1 {
     readGroup(number) {
-        redisClient.XREADGROUP('GROUP', 'clawGroup' + number, 'Ricky', 'COUNT', 100, 'STREAMS', 'clawStream', '>', function(errXREADGROUP, xrg) {
+        let clawWorker1 = this;
+        redisClient.XREADGROUP('GROUP', 'clawGroup' + number, 'Ricky', 'COUNT', 1, 'STREAMS', 'clawStream', '>', function(errXREADGROUP, xrg) {
             if(!errXREADGROUP) {
                 if(xrg != null) {
-                    // xrg: object with one array
-                    // xrg[0]: two arrays
-                    // xrg[0][0]: stream name string
-                    // xrg[0][1]: array with the data
                     let dataArr = xrg[0][1];
                     dataArr.forEach(function(item) {
-                        let actionStr = "XREADGROUP --> clawGroup" + number;
-                        let idStr = "id:" + item[0];
-                        let dataStr = "data: " + item[1][0] + item[1][1];
-                        console.log(actionStr + ", " + idStr + ", " + dataStr);
+                        let id = item[0];
+                        let key = item[1][0];
+                        let value = item[1][1];
+                        console.log("XREADGROUP --> clawGroup" + number + ", id:" + id + ", data: " + key + value);
+                        redisClient.XACK('clawStream', 'clawGroup1', id, function(errXACK, xack) {
+                            if(!errXACK) {
+                                console.log("XACK --> id:" + id + ", " + xack);
+                                console.log("readGroup again\n");
+                                clawWorker1.readGroup(number);
+                            }
+                            else console.error(errXACK);
+                        });
                     });
                 }
-                else console.log("XREADGROUP --> error. clawGroup" + number + " has no data.");
+                else {
+                    console.log("XREADGROUP --> error. clawGroup" + number + " has no data.\n");
+                    console.log("Call blockedReadGroup\n");
+                    clawWorker1.blockedReadGroup(number, 5000);
+                }
             }
             else console.error(errXREADGROUP);
         });
     }
 
-    // listen(workerId) {
-    //     console.log("Listening...");
-    //     let workerQueueName = "workerJS" + workerId + "-queue"; // Queue: workerJS1-queue
-    //     console.log(workerQueueName);
-    //     this.queuePastEvents(workerQueueName);
-    //     this.waitForEvents(workerQueueName);
-    // }
-
-    // queuePastEvents(workerQueueName) {
-    //     let rrpq = this;
-    //     redisClient.LRANGE(workerQueueName, 0, -1, function(errLRANGE, resultLRANGE) {
-    //         if(!errLRANGE) {
-    //             let workerQueueArr = resultLRANGE;
-    //             workerQueueArr.forEach(function() {
-    //                 console.log("workerQueueArr length: " + workerQueueArr.length);
-    //                 console.log("Pop last item in workerQueue, push to the start of workQueue");
-    //                 redisClient.RPOPLPUSH(workerQueueName, rrpq.workQueueName, function(errRPOPLPUSH) {
-    //                     if(!errRPOPLPUSH) console.log("RPOPLPUSH executed.");
-    //                     else console.error(errRPOPLPUSH);
-    //                 });
-    //             });
-    //         }
-    //         else console.error(errLRANGE);
-    //     });
-    // }
-
-    // waitForEvents(workerQueueName) {
-    //     let rrpq = this;
-    //     console.log("\nBlock the connection until the workerQueue gets the task.");
-    //     console.log("waitForEvents...");
-    //
-    //     redisClient.BRPOPLPUSH(rrpq.workQueueName, workerQueueName, 0, function(errBRPOPLPUSH, resultBRPOPLPUSH) {
-    //         if(!errBRPOPLPUSH) {
-    //             console.log("BRPOPLPUSH executed.");
-    //             console.log(resultBRPOPLPUSH);
-    //             redisClient.LREM(workerQueueName, 1, resultBRPOPLPUSH, function(errLREM) {
-    //                 if(!errLREM) {
-    //                     console.log("LREM executed.");
-    //                     rrpq.waitForEvents(workerQueueName);
-    //                 }
-    //                 else console.error(errLREM);
-    //             });
-    //         }
-    //         else console.error(errBRPOPLPUSH);
-    //     });
-    // }
+    blockedReadGroup(number, timeout) {
+        let clawWorker1 = this;
+        redisClient.XREADGROUP('GROUP', 'clawGroup' + number, 'Ricky', 'BLOCK', timeout, 'COUNT', 1, 'STREAMS', 'clawStream', '>', function(errXREADGROUP, xrg) {
+            if(!errXREADGROUP) {
+                if(xrg != null) {
+                    let dataArr = xrg[0][1];
+                    dataArr.forEach(function(item) {
+                        let id = item[0];
+                        let key = item[1][0];
+                        let value = item[1][1];
+                        console.log("XREADGROUP --> BLOCK clawGroup" + number + ", id:" + id + ", data: " + key + value);
+                        redisClient.XACK('clawStream', 'clawGroup1', id, function(errXACK, xack) {
+                            if(!errXACK) {
+                                console.log("XACK --> id:" + id + ", " + xack);
+                                console.log("blockedReadGroup again\n");
+                                clawWorker1.blockedReadGroup(number, timeout);
+                            }
+                        });
+                    });
+                }
+                else {
+                    console.log("XREADGROUP --> BLOCK error. clawGroup" + number + " received no data for " + timeout + "ms.");
+                    process.exit();
+                }
+            }
+            else console.error(errXREADGROUP);
+        });
+    }
 }
 
 let clawWorker1 = new ClawWorker1();
 clawWorker1.readGroup(1);
-
-// ClawWorker1.listen(1);

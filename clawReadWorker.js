f7f01a7f7bb4cacfd0a1edd4a61a76eea58d4c9c
Ricky Dam <RickyDamOttawa@gmail.com>
2019-08-21 16:00:34 -0400

    Implement Redis pipeline for XACK and XDEL

--- clawReadWorker.js
+++ clawReadWorker.js
@@ -12,7 +12,7 @@ class ClawReadWorker {
                         let key = item[1][0];
                         let value = item[1][1];
                         console.log("XREADGROUP --> clawGroup" + groupNumber + ", " + consumer + ", id:" + id + ", data: " + key + value);
-                        clawReadWorker.redisXACK(id, groupNumber, consumer, null);
+                        clawReadWorker.redisPipeline(id, groupNumber, consumer, null);
                     });
                 }
                 else {
@@ -35,7 +35,7 @@ class ClawReadWorker {
                         let key = item[1][0];
                         let value = item[1][1];
                         console.log("XREADGROUP --> BLOCK clawGroup" + groupNumber + ", " + consumer  + ", id:" + id + ", data: " + key + value);
-                        clawReadWorker.redisXACK(id, groupNumber, consumer, timeout);
+                        clawReadWorker.redisPipeline(id, groupNumber, consumer, timeout);
                     });
                 }
                 else {
@@ -47,43 +47,44 @@ class ClawReadWorker {
         });
     }
 
-    redisXACK(id, groupNumber, consumer, timeout) {
-        redis.xack('clawStream', 'clawGroup' + groupNumber, id, function(errXACK, xack) {
+    redisPipeline(id, groupNumber, consumer, timeout) {
+        let pipeline = redis.pipeline();
+        pipeline.xack('clawStream', 'clawGroup' + groupNumber, id, function(errXACK, xack) {
             if(!errXACK) {
                 if(xack === 1) {
                     console.log("XACK --> consumer: " + consumer + ", id: " + id + ", successful.");
-                    clawReadWorker.redisXDEL(id, groupNumber, consumer, timeout);
                 }
                 else {
                     console.log("XACK --> consumer: " + consumer + ", id: " + id + ", failed.");
                     if(timeout == null) {
-                        console.log("readGroup again\n");
+                        console.log("readGroup again");
                         clawReadWorker.readGroup(groupNumber, consumer);
                     }
                     else {
-                        console.log("blockedReadGroup again\n");
+                        console.log("blockedReadGroup again");
                         clawReadWorker.blockedReadGroup(groupNumber, consumer, timeout);
                     }
                 }
             }
         });
-    }
-
-    redisXDEL(id, groupNumber, consumer, timeout) {
-        redis.xdel('clawStream', id, function(errXDEL, xdel) {
+        pipeline.xdel('clawStream', id, function(errXDEL, xdel) {
             if(!errXDEL) {
                 if(xdel === 1) console.log("XDEL --> consumer: " + consumer + ", id: " + id + ", successful.");
                 else console.log("XDEL --> consumer: " + consumer + ", id: " + id + ", failed.");
             }
             else console.error(errXDEL);
             if(timeout == null) {
-                console.log("readGroup again\n");
+                console.log("readGroup again");
                 clawReadWorker.readGroup(groupNumber, consumer);
             }
             else {
-                console.log("blockedReadGroup again\n");
+                console.log("blockedReadGroup again");
                 clawReadWorker.blockedReadGroup(groupNumber, consumer, timeout);
             }
+        });
+        pipeline.exec(function(errPipeline) {
+            if(!errPipeline) console.log("Finished execution of pipeline commands: XACK and XDEL.\n");
+            else console.error(errPipeline);
         });
     }
 }
